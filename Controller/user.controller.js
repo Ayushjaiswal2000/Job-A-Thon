1e9b40b1bf5cb9ad0e96d0d7f6f96e0b18745986
Ayush jaiswal <ayushjaiswal312@gmail.com>
2024-04-26 20:49:22 +0530

    Added Registration part

---
+++ Controller/user.controller.js
@@ -0,0 +1,29 @@
+import { registerUser, authenticateUser } from "../Models/user.model.js";
+
+export default class UserController {
+  getRegister = (req, res, next) => {
+    res.render("user-register");
+  };
+
+  getLogin = (req, res, next) => {
+    res.render("user-login");
+  };
+
+  addUser = (req, res) => {
+    const { name, email, password } = req.body;
+    const newUser = { id: Date.now(), name, email, password };
+    registerUser(newUser);
+    res.status(200).redirect("/login");
+  };
+
+  loginUser = (req, res) => {
+    const { email, password } = req.body;
+    const user = { email, password };
+    const authenticated = authenticateUser(user);
+    if (authenticated) {
+      res.json({ success: true, message: "login successful" });
+    } else {
+      res.json({ success: false, message: "login failed" });
+    }
+  };
+}
--- index.js
+++ index.js
@@ -3,19 +3,26 @@ import path from "path";
 import expressEjsLayouts from "express-ejs-layouts";
 import session from "express-session";
 import HomeController from "./Controller/Home_Controller.js";
+import UserController from "./Controller/user.controller.js";
 
 
 const app = express();
+const homeController =new HomeController();
+const userController = new UserController();
 
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-const homeController =new HomeController();
+// app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(expressEjsLayouts);
 app.set("view engine", "ejs");
 app.set("Views", path.resolve("Views"));
 
 app.get("/",homeController.getHome);
+app.get("/register", userController.getRegister);
+app.post("/register", userController.addUser);
+app.get("/login", userController.getLogin);
+app.post("/login", userController.loginUser);
 
 export default app;
